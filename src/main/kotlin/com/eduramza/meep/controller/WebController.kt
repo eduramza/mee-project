b910c9aca7d837d497173feed840e8cc9826e9c7
eduramza <eduardo.rmb@outlook.com>
2020-06-28 18:28:10 -0300

    adjust in controller

--- src/main/kotlin/com/eduramza/meep/controller/WebController.kt
+++ src/main/kotlin/com/eduramza/meep/controller/WebController.kt
@@ -3,12 +3,10 @@ package com.eduramza.meep.controller
 import com.eduramza.meep.model.Customer
 import com.eduramza.meep.repository.CustomerRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 class WebController {
     @Autowired
     lateinit var repository: CustomerRepository
